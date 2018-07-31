13d872cc58c1cb65a3a27b5a9192a1f375b8ace0
Sergey Papushin <spapushin@gmail.com>
2018-07-31 12:44:19 -0500

    Don't replace swift sail textures

--- tweaks.py
+++ tweaks.py
@@ -388,33 +388,6 @@ def make_sail_behave_like_swift_sail(self):
   # Update the pause menu name for the sail.
   msg = self.bmg.messages_by_id[463]
   msg.string = "Swift Sail"
-  
-  new_sail_tex_image_path = os.path.join(ASSETS_PATH, "swift sail texture.png")
-  new_sail_icon_image_path = os.path.join(ASSETS_PATH, "swift sail icon.png")
-  new_sail_itemget_tex_image_path = os.path.join(ASSETS_PATH, "swift sail item get texture.png")
-  
-  # Modify the sail's texture while sailing.
-  ship_arc = self.get_arc("files/res/Object/Ship.arc")
-  sail_image = ship_arc.get_file("new_ho1.bti")
-  sail_image.replace_image_from_path(new_sail_tex_image_path)
-  sail_image.save_changes()
-  
-  # Modify the sail's item icon.
-  itemicon_arc = self.get_arc("files/res/Msg/itemicon.arc")
-  sail_icon_image = itemicon_arc.get_file("sail_00.bti")
-  sail_icon_image.replace_image_from_path(new_sail_icon_image_path)
-  sail_icon_image.save_changes()
-  
-  # Modify the sail's item get texture.
-  sail_itemget_arc = self.get_arc("files/res/Object/Vho.arc")
-  sail_itemget_model = sail_itemget_arc.get_file("vho.bdl")
-  sail_itemget_tex_image = sail_itemget_model.tex1.textures_by_name["Vho"][0]
-  # Originally it used image format 0xE, which is lossy DXT1 compression.
-  # But implementing this while having the texture actually look good is too difficult, so instead switch this to image format 9 and palette format 1 (C8 with a 255 color RGB565 palette).
-  sail_itemget_tex_image.image_format = 9
-  sail_itemget_tex_image.palette_format = 1
-  sail_itemget_tex_image.replace_image_from_path(new_sail_itemget_tex_image_path)
-  sail_itemget_model.save_changes()
 
 def add_ganons_tower_warp_to_ff2(self):
   # Normally the warp object from Forsaken Fortress down to Ganon's Tower only appears in FF3.
