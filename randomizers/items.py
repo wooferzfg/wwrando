3685d4458f29abb51e462d53f6543059d00997f8
Sergey Papushin <spapushin@gmail.com>
2018-08-05 09:46:51 -0500

    Implement vanilla mode

--- randomizers/items.py
+++ randomizers/items.py
@@ -43,178 +43,112 @@ def randomize_items(self):
 
 def randomize_dungeon_items(self):
   # Places dungeon-specific items first so all the dungeon locations don't get used up by other items.
-  
-  # Temporarily add all progress items except for dungeon keys while we randomize them.
-  items_to_temporarily_add = [
-    item_name for item_name in self.logic.unplaced_progress_items
-    if " Key" not in item_name
-  ]
-  for item_name in items_to_temporarily_add:
-    self.logic.add_owned_item_or_item_group(item_name)
-  
-  # Randomize small keys.
-  small_keys_to_place = [
-    item_name for item_name in (self.logic.unplaced_progress_items + self.logic.unplaced_nonprogress_items)
-    if item_name.endswith(" Small Key")
-  ]
-  previously_accessible_undone_locations = []
-  for item_name in small_keys_to_place:
-    accessible_undone_locations = self.logic.get_accessible_remaining_locations()
-    accessible_undone_locations = [
-      loc for loc in accessible_undone_locations
-      if loc not in self.logic.prerandomization_dungeon_item_locations
-    ]
-    accessible_undone_locations = [
-      loc for loc in accessible_undone_locations
-      if not "Tingle Statue Chest" in self.logic.item_locations[loc]["Types"]
-    ]
-    possible_locations = self.logic.filter_locations_valid_for_item(accessible_undone_locations, item_name)
-    
-    if not possible_locations:
-      raise Exception("No valid locations left to place dungeon items!")
-    
-    location_name = self.rng.choice(possible_locations)
-    self.logic.set_prerandomization_dungeon_item_location(location_name, item_name)
-    
-    self.logic.add_owned_item(item_name) # Temporarily add small keys to the player's inventory while placing them.
-    
-    previously_accessible_undone_locations = accessible_undone_locations
-  
-  # Randomize other dungeon items, big keys, dungeon maps, and compasses.
-  other_dungeon_items_to_place = [
-    item_name for item_name in (self.logic.unplaced_progress_items + self.logic.unplaced_nonprogress_items)
-    if item_name.endswith(" Big Key")
-    or item_name.endswith(" Dungeon Map")
-    or item_name.endswith(" Compass")
-  ]
-  for item_name in other_dungeon_items_to_place:
-    accessible_undone_locations = self.logic.get_accessible_remaining_locations()
-    accessible_undone_locations = [
-      loc for loc in accessible_undone_locations
-      if loc not in self.logic.prerandomization_dungeon_item_locations
-    ]
-    accessible_undone_locations = [
-      loc for loc in accessible_undone_locations
-      if not "Tingle Statue Chest" in self.logic.item_locations[loc]["Types"]
-    ]
-    possible_locations = self.logic.filter_locations_valid_for_item(accessible_undone_locations, item_name)
-    
-    if not possible_locations:
-      raise Exception("No valid locations left to place dungeon items!")
-    
-    location_name = self.rng.choice(possible_locations)
-    self.logic.set_prerandomization_dungeon_item_location(location_name, item_name)
-  
-  # Remove the items we temporarily added.
-  for item_name in items_to_temporarily_add:
-    self.logic.remove_owned_item_or_item_group(item_name)
-  for item_name in small_keys_to_place:
-    self.logic.remove_owned_item(item_name)
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - First Room", "DRC Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - Boarded Up Chest", "DRC Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - Rat Room Boarded Up Chest", "DRC Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - Bird's Nest", "DRC Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Forbidden Woods - Vine Maze Far Chest", "FW Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Tower of the Gods - Hop Across Floating Boxes", "TotG Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Tower of the Gods - Floating Platforms Room", "TotG Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Earth Temple - Transparent Chest in First Crypt", "ET Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Earth Temple - Casket in Second Crypt", "ET Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Earth Temple - End of Foggy Room With Floormasters", "ET Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Wind Temple - Spike Wall Room - First Chest", "WT Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Wind Temple - Chest Behind Seven Armos", "WT Small Key")
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - Big Key Chest", "DRC Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Forbidden Woods - Big Key Chest", "FW Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Tower of the Gods - Big Key Chest", "TotG Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Earth Temple - Big Key Chest", "ET Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Wind Temple - Big Key Chest", "WT Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - Big Key Chest", "DRC Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Forbidden Woods - Big Key Chest", "FW Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Tower of the Gods - Big Key Chest", "TotG Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Earth Temple - Big Key Chest", "ET Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Wind Temple - Big Key Chest", "WT Big Key")
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - Alcove With Water Jugs", "DRC Dungeon Map")
+    self.logic.set_prerandomization_dungeon_item_location("Forbidden Woods - First Room", "FW Dungeon Map")
+    self.logic.set_prerandomization_dungeon_item_location("Tower of the Gods - Chest Behind Bombable Walls", "TotG Dungeon Map")
+    self.logic.set_prerandomization_dungeon_item_location("Forsaken Fortress - Chest Outside Upper Jail Cell", "FF Dungeon Map")
+    self.logic.set_prerandomization_dungeon_item_location("Earth Temple - Transparent Chest In Warp Pot Room", "ET Dungeon Map")
+    self.logic.set_prerandomization_dungeon_item_location("Wind Temple - Chest In Many Cyclones Room", "WT Dungeon Map")
+    self.logic.set_prerandomization_dungeon_item_location("Dragon Roost Cavern - Rat Room", "DRC Compass")
+    self.logic.set_prerandomization_dungeon_item_location("Forbidden Woods - Vine Maze Near Chest", "FW Compass")
+    self.logic.set_prerandomization_dungeon_item_location("Tower of the Gods - Skulls Room Chest", "TotG Compass")
+    self.logic.set_prerandomization_dungeon_item_location("Forsaken Fortress - Chest Guarded By Bokoblin", "FF Compass")
+    self.logic.set_prerandomization_dungeon_item_location("Earth Temple - Chest In Three Blocks Room", "ET Compass")
+    self.logic.set_prerandomization_dungeon_item_location("Wind Temple - Chest In Middle Of Hub Room", "WT Compass")
+
 
 def randomize_progression_items(self):
-  accessible_undone_locations = self.logic.get_accessible_remaining_locations(for_progression=True)
-  if len(accessible_undone_locations) == 0:
-    raise Exception("No progress locations are accessible at the very start of the game!")
+  self.logic.set_location_to_item("Windfall Island - Jail - Tingle - First Gift", "Tingle Tuner")
+  self.logic.set_location_to_item("Forsaken Fortress - Helmaroc King Heart Container", "Spoils Bag")
+  self.logic.set_location_to_item("Dragon Roost Cavern - Mini-Boss", "Grappling Hook")
+  self.logic.set_location_to_item("Fire Mountain - Cave - Chest", "Power Bracelets")
+  self.logic.set_location_to_item("Ice Ring Isle - Cave - Chest", "Iron Boots")
+  self.logic.set_location_to_item("The Great Sea - Beedle's Shop Ship - Bait Bag", "Bait Bag")
+  self.logic.set_location_to_item("Forbidden Woods - Mothula Mini-Boss Room", "Boomerang")
+  self.logic.set_location_to_item("Wind Temple - Wizzrobe Mini-Boss Room", "Hookshot")
+  self.logic.set_location_to_item("Dragon Roost Island - Chest on Top of Boulder", "Delivery Bag")
+  self.logic.set_location_to_item("Windfall Island - Pirate Ship", "Bombs")
+  self.logic.set_location_to_item("Forsaken Fortress - Phantom Ganon", "Skull Hammer")
+  self.logic.set_location_to_item("Forest Haven - On Tree Branch", "Deku Leaf")
+  self.logic.set_location_to_item("Earth Temple - Stalfos Mini-Boss Room", "Mirror Shield")
   
-  # Place progress items.
-  previously_accessible_undone_locations = []
-  while self.logic.unplaced_progress_items:
-    accessible_undone_locations = self.logic.get_accessible_remaining_locations(for_progression=True)
-    
-    if not accessible_undone_locations:
-      raise Exception("No locations left to place progress items!")
-    
-    # If the player gained access to any dungeon item locations, we need to give them those items.
-    newly_accessible_dungeon_item_locations = [
-      loc for loc in accessible_undone_locations
-      if loc in self.logic.prerandomization_dungeon_item_locations
-    ]
-    if newly_accessible_dungeon_item_locations:
-      for dungeon_item_location_name in newly_accessible_dungeon_item_locations:
-        dungeon_item_name = self.logic.prerandomization_dungeon_item_locations[dungeon_item_location_name]
-        self.logic.set_location_to_item(dungeon_item_location_name, dungeon_item_name)
-      
-      continue # Redo this loop iteration with the dungeon item locations no longer being considered 'remaining'.
-    
-    possible_items = self.logic.unplaced_progress_items
-    
-    if not self.options.get("keylunacy"):
-      # Don't randomly place dungeon items, it was already predetermined where they should be placed.
-      possible_items = [
-        item_name for item_name in self.logic.unplaced_progress_items
-        if not self.logic.is_dungeon_item(item_name)
-      ]
-    
-    # Filter out items that are not valid in any of the locations we might use.
-    possible_items = self.logic.filter_items_by_any_valid_location(possible_items, accessible_undone_locations)
-    
-    must_place_useful_item = False
-    should_place_useful_item = True
-    if len(accessible_undone_locations) == 1 and len(possible_items) > 1:
-      # If we're on the last accessible location but not the last item we HAVE to place an item that unlocks new locations.
-      # (Otherwise we will still try to place a useful item, but failing will not result in an error.)
-      must_place_useful_item = True
-    elif len(accessible_undone_locations) >= 17:
-      # If we have a lot of locations open, we don't need to be so strict with prioritizing currently useful items.
-      # This can give the randomizer a chance to place things like Delivery Bag or small keys for dungeons that need x2 to do anything.
-      should_place_useful_item = False
-    
-    # If we wind up placing a useful item it can be a single item or a group.
-    # But if we place an item that is not yet useful, we need to exclude groups.
-    # This is so that a group doesn't wind up taking every single possible remaining location while not opening up new ones.
-    possible_items_when_not_placing_useful = [name for name in possible_items if name not in self.logic.progress_item_groups]
-    # Only exception is when there's exclusively groups left to place. Then we allow groups even if they're not useful.
-    if len(possible_items_when_not_placing_useful) == 0 and len(possible_items) > 0:
-      possible_items_when_not_placing_useful = possible_items
-    
-    if must_place_useful_item or should_place_useful_item:
-      shuffled_list = possible_items.copy()
-      self.rng.shuffle(shuffled_list)
-      item_name = self.logic.get_first_useful_item(shuffled_list, for_progression=True)
-      if item_name is None:
-        if must_place_useful_item:
-          raise Exception("No useful progress items to place!")
-        else:
-          item_name = self.rng.choice(possible_items_when_not_placing_useful)
-    else:
-      item_name = self.rng.choice(possible_items_when_not_placing_useful)
-    
-    if item_name in self.logic.progress_item_groups:
-      # If we're placing an entire item group, we use different logic for deciding the location.
-      # We do not weight towards newly accessible locations.
-      # And we have to select multiple different locations, one for each item in the group.
-      group_name = item_name
-      possible_locations_for_group = accessible_undone_locations.copy()
-      self.rng.shuffle(possible_locations_for_group)
-      self.logic.set_multiple_locations_to_group(possible_locations_for_group, group_name)
-    else:
-      possible_locations = self.logic.filter_locations_valid_for_item(accessible_undone_locations, item_name)
-      
-      # Try to prevent chains of charts that lead to sunken treasures with more charts in them.
-      # If the only locations we have available are sunken treasures we don't have much choice though, so still allow it then.
-      if item_name.startswith("Treasure Chart") or item_name.startswith("Triforce Chart"):
-        possible_locations_without_sunken_treasures = [
-          loc for loc in possible_locations
-          if not "Sunken Treasure" in self.logic.item_locations[loc]["Types"]
-        ]
-        if possible_locations_without_sunken_treasures:
-          possible_locations = possible_locations_without_sunken_treasures
-      
-      # We weight it so newly accessible locations are more likely to be chosen.
-      # This way there is still a good chance it will not choose a new location.
-      possible_locations_with_weighting = []
-      for location_name in possible_locations:
-        if location_name not in previously_accessible_undone_locations:
-          weight = 2
-        else:
-          weight = 1
-        possible_locations_with_weighting += [location_name]*weight
-      
-      location_name = self.rng.choice(possible_locations_with_weighting)
-      self.logic.set_location_to_item(location_name, item_name)
-    
-    previously_accessible_undone_locations = accessible_undone_locations
+  self.logic.set_location_to_item("Greatfish Isle - Sunken Treasure", "Triforce Shard 1")
+  self.logic.set_location_to_item("Gale Isle - Sunken Treasure", "Triforce Shard 2")
+  self.logic.set_location_to_item("Stone Watcher Island - Sunken Treasure", "Triforce Shard 3")
+  self.logic.set_location_to_item("Outset Island - Sunken Treasure", "Triforce Shard 4")
+  self.logic.set_location_to_item("Cliff Plateau Isles - Sunken Treasure", "Triforce Shard 5")
+  self.logic.set_location_to_item("Southern Triangle Island - Sunken Treasure", "Triforce Shard 6")
+  self.logic.set_location_to_item("Seven-Star Isles - Sunken Treasure", "Triforce Shard 7")
+  self.logic.set_location_to_item("Two-Eye Reef - Sunken Treasure", "Triforce Shard 8")
   
+  self.logic.set_location_to_item("Islet of Steel - Interior", "Triforce Chart 1")
+  self.logic.set_location_to_item("Private Oasis - Cabana Labyrinth - Second Chest", "Triforce Chart 2")
+  self.logic.set_location_to_item("Bird's Peak Rock - Cave", "Triforce Chart 3")
+  self.logic.set_location_to_item("The Great Sea - Ghost Ship", "Triforce Chart 4")
+  self.logic.set_location_to_item("Needle Rock Isle - Golden Gunboat", "Triforce Chart 5")
+  self.logic.set_location_to_item("Outset Island - Savage Labyrinth - Floor 30", "Triforce Chart 6")
+  self.logic.set_location_to_item("Stone Watcher Island - Cave", "Triforce Chart 7")
+  self.logic.set_location_to_item("Overlook Island - Cave", "Triforce Chart 8")
+  
+  self.logic.set_location_to_item("Greatfish Isle - Hidden Chest", "Nayru's Pearl")
+  self.logic.set_location_to_item("Dragon Roost Cavern - Gohma Heart Container", "Din's Pearl")
+  self.logic.set_location_to_item("Forbidden Woods - Kalle Demos Heart Container", "Farore's Pearl")
+  
+  self.logic.set_location_to_item("Tower of the Gods - Stone Tablet", "Command Melody")
+  self.logic.set_location_to_item("Dragon Roost Island - Wind Shrine", "Earth God's Lyric")
+  self.logic.set_location_to_item("The Great Sea - Cyclos", "Wind God's Aria")
+  self.logic.set_location_to_item("Windfall Island - Tott", "Song of Passing")
+  
+  self.logic.set_location_to_item("Dragon Roost Island - Rito Aerie - Baito", "Note to Mom")
+  self.logic.set_location_to_item("Windfall Island - Maggie - Letter", "Maggie's Letter")
+  self.logic.set_location_to_item("Windfall Island - Cafe Bar - Postman", "Moblin's Letter")
+  self.logic.set_location_to_item("Windfall Island - Mrs. Marie - Give 21 Joy Pendants", "Cabana Deed")
+  
+  self.logic.set_location_to_item("Two-Eye Reef - Big Octo Great Fairy", "Magic Meter Upgrade")
+
+  self.logic.set_location_to_item("Diamond Steppe Island - Warp Maze Cave - Second Chest", "Ghost Ship Chart")
+  
+  self.logic.set_location_to_item("Tower of the Gods - Gohdan Heart Container", "Progressive Sword")
+  self.logic.set_location_to_item("Earth Temple - Jalhalla Heart Container", "Progressive Sword")
+  self.logic.set_location_to_item("Wind Temple - Molgera Heart Container", "Progressive Sword")
+  
+  self.logic.set_location_to_item("Tower of the Gods - Darknut Mini-Boss Room", "Progressive Bow")
+  self.logic.set_location_to_item("Mother and Child Isles - Inside Mother Isle", "Progressive Bow")
+  self.logic.set_location_to_item("Ganon's Tower - Maze Chest", "Progressive Bow")
+  
+  self.logic.set_location_to_item("Northern Fairy Island - Great Fairy", "Progressive Wallet")
+  self.logic.set_location_to_item("Outset Island - Great Fairy", "Progressive Wallet")
+  
+  self.logic.set_location_to_item("Windfall Island - Jail - Maze Chest", "Progressive Picto Box")
+  self.logic.set_location_to_item("Windfall Island - Lenzo's House - Bring Forest Firefly", "Progressive Picto Box")
+  
+  self.logic.set_location_to_item("Windfall Island - Mila", "Empty Bottle")
+  self.logic.set_location_to_item("Bomb Island - Submarine", "Empty Bottle")
+  self.logic.set_location_to_item("Rock Spire Isle - Beedle's Special Shop Ship - 500 Rupee Item", "Empty Bottle")
+  self.logic.set_location_to_item("Rock Spire Isle - Beedle's Special Shop Ship - 950 Rupee Item", "Empty Bottle")
+
   # Make sure locations that should have dungeon items in them have them properly placed, even if the above logic missed them for some reason.
   for location_name in self.logic.prerandomization_dungeon_item_locations:
     if location_name in self.logic.remaining_item_locations:
@@ -224,9 +158,6 @@ def randomize_progression_items(self):
   game_beatable = self.logic.check_requirement_met("Can Reach and Defeat Ganondorf")
   if not game_beatable:
     raise Exception("Game is not beatable on this seed! This error shouldn't happen.")
-
-
-
 
 def write_changed_items(self):
   for location_name, item_name in self.logic.done_item_locations.items():
