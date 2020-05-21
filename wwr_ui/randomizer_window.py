41960dbe4aa578c4e8bdb318a0aa0e6dfaf084e5
wooferzfg <spapushin@gmail.com>
2020-05-21 18:13:02 -0500

    Always run in bulk

--- wwr_ui/randomizer_window.py
+++ wwr_ui/randomizer_window.py
@@ -44,7 +44,7 @@ class WWRandomizerWindow(QMainWindow):
     self.randomizer_thread = None
     
     self.cmd_line_args = cmd_line_args
-    self.bulk_test = ("-bulk" in cmd_line_args)
+    self.bulk_test = True
     self.no_ui_test = ("-noui" in cmd_line_args)
     self.profiling = ("-profile" in cmd_line_args)
     
